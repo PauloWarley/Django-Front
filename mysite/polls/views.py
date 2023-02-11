07763ac53c57cfa2d9cffc181c0483b58bfd5a8c
Paulo Reis <92232479+PauloWarley@users.noreply.github.com>
2023-02-11 15:24:19 -0300

    Projeto Completo

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,22 +1,51 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    
+    return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    HttpResponse("Essa é pergunta de numero " + question_id)
     
 def results(request, question_id):
-    response = "Esses são os resultados da pergunda de numero " + question_id
+    # Recupera a pergunta pelo seu id
+    question = Question.objects.get(pk=question_id)
+
+    # Recupera todas as escolhas relacionadas a essa pergunta
+    choices = question.choice_set.all()
     
-    return HttpResponse(response)
+    context = {'question': question}
+    
+    return render(request, 'polls/results.html', context)
+
 
 def vote (request, question_id):
-    response = "Você votou na questão " + question_id
+    question = get_object_or_404(Question, pk=question_id)
     
-    return HttpResponse(response)
+    try:
+        print(question_id)
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        
+    
+    except KeyError:
+        return render(request, 'polls/vote.html', {
+            'question': question,
+            'erro_message': "You didn't select a choice."
+            
+        })
+        
+        
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=[question.id]))
+        
 
 # Parei em 1:30
