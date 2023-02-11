adb266e75989fa2b816b0b5a25a65316b094d323
Paulo Reis <92232479+PauloWarley@users.noreply.github.com>
2023-02-11 10:53:23 -0300

    iniciando primeira view

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'Americas/Sao_Paulo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,3 +5,18 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def detail(request, question_id):
+    HttpResponse("Essa é pergunta de numero " + question_id)
+    
+def results(request, question_id):
+    response = "Esses são os resultados da pergunda de numero " + question_id
+    
+    return HttpResponse(response)
+
+def vote (request, question_id):
+    response = "Você votou na questão " + question_id
+    
+    return HttpResponse(response)
+
+# Parei em 1:30
